ec1cc4e4f5c07f4e42b94252e2428e56bc6edcd6
miniluz <javiermelon4fu@gmail.com>
2024-03-29 23:38:03 +0100

    Add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use tracing::{info, warn};
+
+fn main() -> color_eyre::eyre::Result<()> {
+    color_eyre::install()?;
+
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    info!("Just before hello world!");
+
     println!("Hello, world!");
+
+    warn!("Just after hello world!");
+
+    Ok(())
 }
