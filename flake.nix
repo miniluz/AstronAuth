fb558bef639eaa490f9fabe04e089faa69e5922c
miniluz <javiermelon4fu@gmail.com>
2024-03-30 00:05:34 +0100

    Remove bacon from buildInputs

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
           };
           devShells.default = mkShell {
             inherit nativeBuildInputs;
-            buildInputs = [ bacon ];
+            buildInputs = [ ];
             RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
           };
         }
