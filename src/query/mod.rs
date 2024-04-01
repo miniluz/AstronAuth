49c8e5cabd72131c6818f077fefb908b58d22745
miniluz <javiermelon4fu@gmail.com>
2024-04-01 18:07:09 +0200

    Restructured, tests

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -1,14 +1,21 @@
 //! This module is necessary to handle the requirements the RFC has for preserving the query
 
-use poem::{error::ResponseError, http::StatusCode, FromRequest, Request, Result};
+use poem::{
+    error::ResponseError,
+    http::{StatusCode, Uri},
+    FromRequest, Request, Response,
+};
 use tracing::instrument;
+
+#[cfg(test)]
+mod test;
 
 pub const RESPONSE_TYPE_PARAM: &str = "response_type";
 pub type ResponseType = String;
 pub const CLIENT_ID_PARAM: &str = "client_id";
 pub type ClientId = String;
 pub const REDIRECT_URI_PARAM: &str = "redirect_uri";
-pub type RedirectUri = String;
+pub type RedirectUri = Uri;
 pub const SCOPE_PARAM: &str = "scope";
 pub type ScopeList = Vec<Scope>;
 pub const STATE_PARAM: &str = "state";
@@ -17,6 +24,7 @@ pub type State = String;
 /// A scope is a valid scope according to
 /// [section 3.3](https://datatracker.ietf.org/doc/html/rfc6749#section-3.3)
 /// of the RFC
+#[derive(PartialEq, Eq)]
 pub struct Scope(String);
 
 impl std::fmt::Debug for Scope {
@@ -52,45 +60,58 @@ impl Into<String> for Scope {
 }
 
 /// Represents the authorization request query.
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct AuthorizationRequestQuery {
     /// All the parameters that aren't client, redirect_uri, scope and state are preserved as-is.
-    /// They're not percent-decoded.
-    opaque_parameters: Vec<String>,
+    opaque_parameters: Vec<(String, String)>,
     pub client_id: ClientId,
     pub redirect_uri: RedirectUri,
     pub scope: ScopeList,
     pub state: Option<State>,
 }
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, thiserror::Error, PartialEq)]
 #[error("authorization query parsing error")]
 pub enum AuthorizationQueryParsingError {
     #[error("only the \"code\" response_type is supported")]
     UnsupportedResponseType,
-    #[error("parameter `{0:?}` sent more than once")]
+    #[error("parameter {0:?} sent more than once")]
     RepeatedParameter(&'static str),
-    #[error("problem parsing percent encoding")]
-    ParsingError(#[from] std::str::Utf8Error),
-    #[error("missing parameter `{0:?}`")]
+    #[error("query must be in application/x-www-form-urlencoded format")]
+    ParsingError(#[from] serde_urlencoded::de::Error),
+    #[error("missing parameter {0:?}")]
     MissingParameter(&'static str),
     #[error(
         "invalid scope format on `{0:?}`. check section 3.3 of RFC 6749 for the allowed characters"
     )]
     InvalidScope(String),
+    // TODO: use
+    #[error("the redirect_uri is invalid")]
+    InvalidUri,
 }
 
 impl ResponseError for AuthorizationQueryParsingError {
+    // TODO: Implement actual statuses
     fn status(&self) -> StatusCode {
-        StatusCode::BAD_REQUEST
+        match self {
+            Self::MissingParameter(REDIRECT_URI_PARAM) | Self::InvalidUri => {
+                StatusCode::BAD_REQUEST
+            }
+            _ => StatusCode::SEE_OTHER,
+        }
+    }
+
+    fn as_response(&self) -> Response {
+        Response::builder()
+            .status(self.status())
+            .body(self.to_string())
     }
 }
 
-/// Implementations for parsing
-impl AuthorizationRequestQuery {
+impl std::str::FromStr for AuthorizationRequestQuery {
+    type Err = AuthorizationQueryParsingError;
     /// Tries to generate itself from a still percentage-encoded string.
-    #[instrument(skip_all)]
-    fn try_from_query(percent_encoded_query: &str) -> Result<Self, AuthorizationQueryParsingError> {
+    fn from_str(query: &str) -> Result<Self, Self::Err> {
         use AuthorizationQueryParsingError as Error;
 
         #[derive(Debug)]
@@ -133,55 +154,50 @@ impl AuthorizationRequestQuery {
                 match self {
                     Self::Some(_) | Self::None => Err(Error::RepeatedParameter(SCOPE_PARAM)),
                     Self::Undefined => {
-                        if value != "" {
+                        if value == "" {
+                            Ok(Self::None)
+                        } else {
                             Ok(Self::Some(
                                 value
                                     .split(' ')
                                     .map(Scope::try_from)
                                     .collect::<Result<Vec<Scope>, Error>>()?,
                             ))
-                        } else {
-                            Ok(Self::None)
                         }
                     }
                 }
             }
         }
 
-        let mut opaque_parameters: Vec<String> = vec![];
+        impl OptionUndefined<Uri> {
+            fn try_define(self, value: String) -> std::result::Result<Self, Error> {
+                match self {
+                    Self::Some(_) | Self::None => Err(Error::RepeatedParameter(REDIRECT_URI_PARAM)),
+                    Self::Undefined => {
+                        if value == "" {
+                            Ok(Self::None)
+                        } else {
+                            Ok(Self::Some(
+                                value.parse::<Uri>().map_err(|_| Error::InvalidUri)?,
+                            ))
+                        }
+                    }
+                }
+            }
+        }
+
+        let mut opaque_parameters: Vec<(String, String)> = vec![];
         let mut response_type: OptionUndefined<ResponseType> = OptionUndefined::Undefined;
         let mut client_id: OptionUndefined<ClientId> = OptionUndefined::Undefined;
         let mut redirect_uri: OptionUndefined<RedirectUri> = OptionUndefined::Undefined;
         let mut scope: OptionUndefined<ScopeList> = OptionUndefined::Undefined;
         let mut state: OptionUndefined<State> = OptionUndefined::Undefined;
 
-        for query_part in percent_encoded_query.split('&') {
-            let (encoded_key, encoded_value) =
-                if let Some((key, value)) = query_part.split_once('=') {
-                    (key, value)
-                } else {
-                    opaque_parameters.push(query_part.to_owned());
-                    continue;
-                };
+        let key_value_pairs: Vec<(String, String)> =
+            serde_urlencoded::from_str(query).map_err(|err| Error::ParsingError(err))?;
 
-            match encoded_key {
-                RESPONSE_TYPE_PARAM | CLIENT_ID_PARAM | REDIRECT_URI_PARAM | SCOPE_PARAM
-                | STATE_PARAM => {}
-                _ => {
-                    opaque_parameters.push(query_part.to_owned());
-                    continue;
-                }
-            }
-
-            // note that here key must already be decoded because it matched the decoded consts.
-            let key = encoded_key;
-            let value =
-                match percent_encoding::percent_decode(encoded_value.as_bytes()).decode_utf8() {
-                    Ok(str) => str.to_string(),
-                    Err(err) => return Err(Error::ParsingError(err)),
-                };
-
-            match key {
+        for (key, value) in key_value_pairs {
+            match &*key {
                 RESPONSE_TYPE_PARAM => {
                     response_type = response_type.try_define(value, RESPONSE_TYPE_PARAM)?;
                 }
@@ -189,7 +205,7 @@ impl AuthorizationRequestQuery {
                     client_id = client_id.try_define(value, CLIENT_ID_PARAM)?;
                 }
                 REDIRECT_URI_PARAM => {
-                    redirect_uri = redirect_uri.try_define(value, REDIRECT_URI_PARAM)?;
+                    redirect_uri = redirect_uri.try_define(value)?;
                 }
                 SCOPE_PARAM => {
                     scope = scope.try_define(value)?;
@@ -197,7 +213,9 @@ impl AuthorizationRequestQuery {
                 STATE_PARAM => {
                     state = state.try_define(value, STATE_PARAM)?;
                 }
-                _ => unreachable!(),
+                _ => {
+                    opaque_parameters.push((key, value));
+                }
             }
         }
 
@@ -220,12 +238,13 @@ impl AuthorizationRequestQuery {
             state
         );
 
-        if let OptionUndefined::Some(response_type) = response_type {
-            if response_type != "code" {
+        match response_type.to_option() {
+            None => return Err(Error::MissingParameter(RESPONSE_TYPE_PARAM)),
+            Some(s) if s != "code" => {
                 return Err(Error::UnsupportedResponseType);
             }
-        } else {
-            return Err(Error::MissingParameter(RESPONSE_TYPE_PARAM));
+            // Some and s == code
+            _ => {}
         }
 
         let client_id = match client_id.to_option() {
@@ -250,7 +269,10 @@ impl AuthorizationRequestQuery {
 
         Ok(result)
     }
+}
 
+/// Implementations for parsing
+impl AuthorizationRequestQuery {
     /// Simply maps to `Self::try_from_query`
     #[instrument(name = "parse_authorization_query", skip_all)]
     async fn internal_from_request(req: &Request) -> Result<Self, AuthorizationQueryParsingError> {
@@ -259,7 +281,7 @@ impl AuthorizationRequestQuery {
 
         tracing::trace!("Started to parse query: {:?}", query);
 
-        let result = Self::try_from_query(query);
+        let result = query.parse();
 
         tracing::trace!("Resulted in: {:?}", result);
 
@@ -269,7 +291,7 @@ impl AuthorizationRequestQuery {
 
 impl<'a> FromRequest<'a> for AuthorizationRequestQuery {
     /// Simply maps to `Self::internal_from_request`
-    async fn from_request(req: &'a Request, _body: &mut poem::RequestBody) -> Result<Self> {
+    async fn from_request(req: &'a Request, _body: &mut poem::RequestBody) -> poem::Result<Self> {
         Self::internal_from_request(req).await.map_err(Into::into)
     }
 }
---
+++ src/query/test.rs
@@ -0,0 +1,272 @@
+use poem::http::Uri;
+
+use super::{
+    AuthorizationQueryParsingError, AuthorizationRequestQuery, CLIENT_ID_PARAM, REDIRECT_URI_PARAM,
+    RESPONSE_TYPE_PARAM, SCOPE_PARAM, STATE_PARAM,
+};
+
+use AuthorizationQueryParsingError as Error;
+
+#[cfg(test)]
+fn parse_authorization_query(query: &str) -> Result<AuthorizationRequestQuery, Error> {
+    query.parse()
+}
+
+#[test]
+fn trivial_query() {
+    let trivial_query = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+        ("scope", "scope_a scope_b"),
+    ])
+    .unwrap();
+
+    let trivial_auth_query = AuthorizationRequestQuery {
+        opaque_parameters: vec![],
+        client_id: "valid_client_id".to_owned(),
+        redirect_uri: Uri::from_static("https://example.org/foo/bar?hey=now&test"),
+        scope: vec!["scope_a".try_into().unwrap(), "scope_b".try_into().unwrap()],
+        state: None,
+    };
+
+    assert_eq!(
+        parse_authorization_query(&trivial_query).unwrap(),
+        trivial_auth_query
+    );
+}
+
+#[test]
+fn missing_parameters() {
+    // missins response type
+    let missing_response_type = serde_urlencoded::to_string([
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&missing_response_type),
+        Err(Error::MissingParameter(RESPONSE_TYPE_PARAM))
+    );
+
+    // parameters without valuesmust be treated as unsent as per section 3.1. of RFC 6749
+    let missing_response_type = serde_urlencoded::to_string([
+        ("response_type", ""),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&missing_response_type),
+        Err(Error::MissingParameter(RESPONSE_TYPE_PARAM))
+    );
+
+    // missing client id
+    let missing_client_id = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&missing_client_id),
+        Err(Error::MissingParameter(CLIENT_ID_PARAM))
+    );
+
+    let missing_client_id = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("client_id", ""),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&missing_client_id),
+        Err(Error::MissingParameter(CLIENT_ID_PARAM))
+    );
+
+    // missing redirect_uri
+    let missing_redirect_uri =
+        serde_urlencoded::to_string([("response_type", "code"), ("client_id", "valid_client_id")])
+            .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&missing_redirect_uri),
+        Err(Error::MissingParameter(REDIRECT_URI_PARAM))
+    );
+
+    let missing_redirect_uri = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", ""),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&missing_redirect_uri),
+        Err(Error::MissingParameter(REDIRECT_URI_PARAM))
+    );
+}
+
+#[test]
+fn invalid_parameters() {
+    let invalid_redirect_uri = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "ht/tps://example.org/foo/bar?hey=now&test"),
+        ("scope", "scope_a scope_b"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&invalid_redirect_uri),
+        Err(Error::InvalidUri)
+    );
+}
+
+#[test]
+fn invalid_scope() {
+    let invalid_scope = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+        ("scope", "invalid_scope_à"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&invalid_scope),
+        Err(Error::InvalidScope("invalid_scope_à".to_owned()))
+    );
+}
+
+#[test]
+fn ignore_opaque_parameters() {
+    let repeated_opaque_params = serde_urlencoded::to_string([
+        ("repeated1", "hey"),
+        ("response_type", "code"),
+        ("repeated1", ""),
+        ("client_id", "valid_client_id"),
+        ("repeated2", "once"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+        ("scope", "scope_a scope_b"),
+        ("repeated2", "twice"),
+        ("repeated2", "thrice"),
+    ])
+    .unwrap();
+
+    let repeated_opaque_params_query = AuthorizationRequestQuery {
+        opaque_parameters: vec![
+            ("repeated1".to_owned(), "hey".to_owned()),
+            ("repeated1".to_owned(), "".to_owned()),
+            ("repeated2".to_owned(), "once".to_owned()),
+            ("repeated2".to_owned(), "twice".to_owned()),
+            ("repeated2".to_owned(), "thrice".to_owned()),
+        ],
+        client_id: "valid_client_id".to_owned(),
+        redirect_uri: Uri::from_static("https://example.org/foo/bar?hey=now&test"),
+        scope: vec!["scope_a".try_into().unwrap(), "scope_b".try_into().unwrap()],
+        state: None,
+    };
+
+    assert_eq!(
+        parse_authorization_query(&repeated_opaque_params).unwrap(),
+        repeated_opaque_params_query
+    );
+}
+
+#[test]
+fn repeated_parameters() {
+    let repeated_response_type = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("response_type", ""),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+        ("scope", "scope_a scope_b"),
+        ("state", "opaque"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&repeated_response_type),
+        Err(Error::RepeatedParameter(RESPONSE_TYPE_PARAM))
+    );
+
+    let repeated_client_id = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("client_id", "valid_client_id"),
+        ("client_id", ""),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+        ("scope", "scope_a scope_b"),
+        ("state", "opaque"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&repeated_client_id),
+        Err(Error::RepeatedParameter(CLIENT_ID_PARAM))
+    );
+
+    let repeated_redirect_uri = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+        ("redirect_uri", ""),
+        ("scope", "scope_a scope_b"),
+        ("state", "opaque"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&repeated_redirect_uri),
+        Err(Error::RepeatedParameter(REDIRECT_URI_PARAM))
+    );
+
+    let repeated_scope = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+        ("scope", "scope_a scope_b"),
+        ("scope", ""),
+        ("state", "opaque"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&repeated_scope),
+        Err(Error::RepeatedParameter(SCOPE_PARAM))
+    );
+
+    let repeated_state = serde_urlencoded::to_string([
+        ("response_type", "code"),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+        ("scope", "scope_a scope_b"),
+        ("state", "opaque"),
+        ("state", ""),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&repeated_state),
+        Err(Error::RepeatedParameter(STATE_PARAM))
+    );
+}
+
+#[test]
+fn unsupported_response_type() {
+    let unsupported_response_type = serde_urlencoded::to_string([
+        ("response_type", "not_code"),
+        ("client_id", "valid_client_id"),
+        ("redirect_uri", "https://example.org/foo/bar?hey=now&test"),
+        ("scope", "scope_a scope_b"),
+    ])
+    .unwrap();
+
+    assert_eq!(
+        parse_authorization_query(&unsupported_response_type),
+        Err(Error::UnsupportedResponseType)
+    );
+}
